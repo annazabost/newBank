b45777bde17386f1874f04bfe35129b1bf91fffd
Anna Zabost <annazabost404@gmail.com>
2019-03-22 16:23:39 +0100

    4

--- src/premiumAccount.java
+++ src/premiumAccount.java
@@ -1,5 +1,12 @@
 public class premiumAccount extends Account {
 
-    
+    private Integer bonus;
 
+    public final Integer getBonus() {
+        return bonus;
+    }
+
+    public premiumAccount(Integer bonus) {
+        this.bonus = bonus;
+    }
 }
---
+++ src/savingsAccount.java
@@ -0,0 +1,12 @@
+public class savingsAccount extends Account {
+
+    private Integer savingsMoney;
+
+    public Integer getSavingsMoney() {
+        return savingsMoney;
+    }
+
+    public void setSavingsMoney(Integer savingsMoney) {
+        this.savingsMoney = savingsMoney;
+    }
+}
