97e1b79f1a4c40bddbd7f3c768f681a073a1ef39
Anna Zabost <annazabost404@gmail.com>
2019-03-22 16:05:24 +0100

    Dziala

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        StandardAccount l = new StandardAccount();
-        l.lololo(6666);
+
     }
 }
--- src/StandardAccount.java
+++ src/StandardAccount.java
@@ -1,7 +1,4 @@
 public class StandardAccount extends Account {
 
-
-    public void lololo(Integer balance) {
-        System.out.println(balance);
-    }
 }
+
